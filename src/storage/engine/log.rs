ba477bdab9c0187943e4adb59b9e916e49ae86c5
ruanjiancheng <57210517+ruanjiancheng@users.noreply.github.com>
2023-12-24 19:34:44 +0800

    Feature/mvcc (#3)
    * feat: bitcask engine
    
    * feat: bitcask engine
    
    * feat: Error, serialize and deserialize
    
    * feat: mvcc
    
    * feat: add test
    
    * delete println

--- src/storage/engine/log.rs
+++ src/storage/engine/log.rs
@@ -1,5 +1,7 @@
 use std::{path::PathBuf, fs::{File, self}, io::{BufReader, Seek, SeekFrom, Read, BufWriter, Write}};
 
+use crate::error::Result;
+
 use super::KeyDir;
 
 pub(crate) struct Log {
@@ -8,7 +10,7 @@ pub(crate) struct Log {
 }
 
 impl Log {
-    pub(crate) fn new(path: PathBuf) -> crate::Result<Log> {
+    pub(crate) fn new(path: PathBuf) -> Result<Log> {
         if let Some(path) = path.parent() {
             fs::create_dir_all(path)?;
         }
@@ -16,55 +18,66 @@ impl Log {
         Ok(Log { path, file })
     }
 
-    pub(crate) fn build_keydir(&mut self) -> crate::Result<KeyDir> {
-        let file_len = self.file.metadata()?.len();
-
-        let mut len_buf = [0 as u8; 4];
+    pub(crate) fn build_keydir(&mut self) -> Result<KeyDir> {
+        let mut len_buf = [0u8; 4];
         let mut keydir = KeyDir::new();
-
-        let mut reader = BufReader::new(&mut self.file);
-        let pos = reader.seek(SeekFrom::Start(0))?;
+        let file_len = self.file.metadata()?.len();
+        let mut r = BufReader::new(&mut self.file);
+        let mut pos = r.seek(SeekFrom::Start(0))?;
 
         while pos < file_len {
-            reader.read_exact(&mut len_buf)?;
-            let key_len = u32::from_be_bytes(len_buf);
+            let result = || -> std::result::Result<(Vec<u8>, u64, Option<u32>), std::io::Error> {
+                r.read_exact(&mut len_buf)?;
+                let key_len = u32::from_be_bytes(len_buf);
+                r.read_exact(&mut len_buf)?;
+                let value_len_or_tombstone = match i32::from_be_bytes(len_buf) {
+                    l if l >= 0 => Some(l as u32),
+                    _ => None,
+                };
 
-            reader.read_exact(&mut len_buf)?;
-            let value_len_or_tombstone = match i32::from_be_bytes(len_buf) {
-                -1 => None,
-                value_len => Some(value_len as u32), 
-            };
+                // | key len 4bytes | value len 4bytes | key | value |
+                //                                           ^
+                //                                        value_pos
+                let value_pos = pos + 4 + 4 + key_len as u64;
 
-            // | key len 4bytes | value len 4bytes | key | value |
-            //                                           ^
-            //                                        value_pos
-            let value_pos = pos + 4 + 4 + key_len as u64;
+                let mut key = vec![0; key_len as usize];
+                r.read_exact(&mut key)?;
 
-            let mut key = vec![0; key_len as usize];
-            reader.read_exact(&mut key)?;
-
-            match value_len_or_tombstone {
-                Some(value_len) => {
-                    if value_len as u64 + value_pos > file_len {
-                        log::error!("Found incomplete entry at offset {}, truncating file", pos);
-                        return Err(
-                            "value extends beyond end of file".into()
-                        );
-                    } else {
-                        reader.seek_relative(value_len as i64)?;
-                        keydir.insert(key, (value_pos, value_len));
+                if let Some(value_len) = value_len_or_tombstone {
+                    if value_pos + value_len as u64 > file_len {
+                        return Err(std::io::Error::new(
+                            std::io::ErrorKind::UnexpectedEof,
+                            "value extends beyond end of file",
+                        ));
                     }
-                },
-                None => {
-                    keydir.remove(&key);
+                    r.seek_relative(value_len as i64)?; // avoids discarding buffer
                 }
+
+                Ok((key, value_pos, value_len_or_tombstone))
+            }();
+
+            match result {
+                Ok((key, value_pos, Some(value_len))) => {
+                    keydir.insert(key, (value_pos, value_len));
+                    pos = value_pos + value_len as u64;
+                }
+                Ok((key, value_pos, None)) => {
+                    keydir.remove(&key);
+                    pos = value_pos;
+                }
+                Err(err) if err.kind() == std::io::ErrorKind::UnexpectedEof => {
+                    log::error!("Found incomplete entry at offset {}, truncating file", pos);
+                    self.file.set_len(pos)?;
+                    break;
+                }
+                Err(err) => return Err(err.into()),
             }
         }
 
         Ok(keydir)
     }
 
-    pub(crate) fn read_value(&mut self, value_pos: u64, value_len: u32) -> crate::Result<Vec<u8>> {
+    pub(crate) fn read_value(&mut self, value_pos: u64, value_len: u32) -> Result<Vec<u8>> {
         let mut value_buf = vec![0; value_len as usize];
         self.file.seek(SeekFrom::Start(value_pos))?;
         self.file.read_exact(&mut value_buf)?;
@@ -73,7 +86,7 @@ impl Log {
     }
 
     /// 将 key value 写入 log 文件中，返回记录起始位置 pos 和记录的长度 len
-    pub(crate) fn write_entry(&mut self, key: &[u8], value: Option<&[u8]>) -> crate::Result<(u64, u32)> {
+    pub(crate) fn write_entry(&mut self, key: &[u8], value: Option<&[u8]>) -> Result<(u64, u32)> {
         let key_len = key.len() as u32;
         let value_len = value.map_or(0, |v| v.len() as u32);
         let value_len_or_tombstone = value.map_or(-1, |v| v.len() as i32);
@@ -94,11 +107,11 @@ impl Log {
         Ok((pos, len))
     }
 
-    pub(crate) fn total_size(&self) -> crate::Result<u64> {
+    pub(crate) fn total_size(&self) -> Result<u64> {
         Ok(self.file.metadata()?.len())
     }
 
-    pub(crate) fn flush(&self) -> crate::Result<()> {
+    pub(crate) fn flush(&self) -> Result<()> {
         self.file.sync_all()?;
         Ok(())
     }
